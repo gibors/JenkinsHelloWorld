123dc65ee54bf35bedbec83027049f4541cc73e7
gibors <giresa.ico@gmail.com>
2020-04-08 21:31:45 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 	public static void main(String[] args) {
 
-		System.out.println("This is my firs Jenkins java implementation");
+		System.out.println("***************This is my firs Jenkins java implementation****************");
 	}
 
 }
